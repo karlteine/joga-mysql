17f7b17406a2caadf495aa16aeb9ec40802c4f7f
karlteine <karlkivisild@gmail.com>
2023-12-12 09:56:30 +0200

    Add project files

---
+++ utils/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
+
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/'
+}))
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+module.exports = app
