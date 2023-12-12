396090cf804e1cbc9ac94d6cc422cec0f949821e
karlteine <karlkivisild@gmail.com>
2023-12-12 11:19:03 +0200

    As an user i can see single yoga article [Delivers #186663226]

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./utils/app')
 const articleRoutes = require('./routes/articles')
 app.use('/', articleRoutes)
+app.use('/article', articleRoutes)
 
 app.listen(3010, () => {
     console.log('server is connected at port 3010')
